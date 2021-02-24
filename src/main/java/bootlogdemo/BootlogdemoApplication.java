6632f25fa895e6f97992c493dd59814ecbd4b480
EricTownsChina <840017241@qq.com>
2021-02-24 16:59:18 +0800

    first commit

---
+++ src/main/java/bootlogdemo/BootlogdemoApplication.java
@@ -0,0 +1,13 @@
+package bootlogdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootlogdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootlogdemoApplication.class, args);
+    }
+
+}
